9db92da0b6886c549b53f35703073c774192699a
Lucas Stephan <lucas.stephan@epita.fr>
2023-05-23 17:44:08 +0200

    feat:mobile website update

--- scripts/mobile.js
+++ scripts/mobile.js
@@ -1,3 +1,22 @@
+
+//  Initialize Swiper
+let swiper = new Swiper(".members-swiper", {
+    effect: "cards",
+    grabCursor: true,
+});
+
+// Listen to swiper click event
+const cardClickHandler = (index) => {
+    const cards = document.getElementsByClassName('swiper-slide');
+}
+
+const cardsPicto = document.getElementsByClassName('picto-container');
+for (let i = 0; i < cardsPicto.length; i++)
+    cardsPicto.item(i).addEventListener('click', cardClickHandler);
+
+
+// INIT
+
 const init = () => {
    
     // TITLE ANIMATION
